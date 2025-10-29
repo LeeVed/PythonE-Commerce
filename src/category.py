b34679e3ffdb873375587b4248ac1f1646a42679
LeeVed <leeveden2008@gmail.com>
2025-10-29 20:58:31 +0300

    getter setter addition

--- src/category.py
+++ src/category.py
@@ -16,7 +16,26 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         # активируем подсчет атрибутов класса:категорий и продуктов
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product: Product) -> None:
+            """Метод экземпляра добавляет товар в категорию"""
+            self.__products.append(product)
+            Category.product_count += 1
+
+
+    @property
+    def products(self) -> str:
+        """Геттер для вывода списка товаров в строке нужного формата"""
+        if not self.__products:
+            return "В этой категории пока нет товаров"
+
+        products_list = []
+        for product in self.__products:
+            products_list.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+
+        return "\n".join(products_list)
