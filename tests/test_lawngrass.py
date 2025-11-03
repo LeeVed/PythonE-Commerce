36f149f9708436cbb7bb20fdd2f2c6494c2ead5e
LeeVed <leeveden2008@gmail.com>
2025-11-03 23:14:05 +0300

    final fixes

--- tests/test_lawngrass.py
+++ tests/test_lawngrass.py
@@ -4,6 +4,7 @@ from src.lawngrass import LawnGrass
 
 
 def test_lawngrass_init(lawngrass1: LawnGrass) -> None:
+    """Тест на проверку инициализации"""
     assert lawngrass1.name == "Газонная трава"
     assert lawngrass1.description == "Элитная трава для газона"
     assert lawngrass1.price == 500.0
@@ -14,10 +15,11 @@ def test_lawngrass_init(lawngrass1: LawnGrass) -> None:
 
 
 def test_lawngrass_add_successful(lawngrass1: LawnGrass, lawngrass2: LawnGrass) -> None:
-    """Тест на проверку складывание объектов одного класса"""
+    """Тест на проверку корректного складывания объектов одного класса"""
     assert lawngrass1 + lawngrass2 == 16750.0
 
 
 def test_lawngrass_add_error(lawngrass1: LawnGrass, lawngrass2: LawnGrass) -> None:
+    """Тест на проверку ошибки при сложении разных типов объектов"""
     with pytest.raises(TypeError):
         result = lawngrass1 + 2  # type: ignore
