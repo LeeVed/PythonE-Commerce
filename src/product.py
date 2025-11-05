353c14ff79e93b6e7d08fa0c8678d9575d48af44
LeeVed <leeveden2008@gmail.com>
2025-11-05 18:37:01 +0300

    new modules and fixes

--- src/product.py
+++ src/product.py
@@ -1,7 +1,10 @@
 from typing import Any
 
+from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
 
-class Product:
+
+class Product(BaseProduct, PrintMixin):
     """Класс для описания продуктов"""
 
     name: str
@@ -15,6 +18,7 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self) -> str:
         """Строковое представление в следующем виде: Название продукта, 80 руб. Остаток: 15 шт."""
